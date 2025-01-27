15c5b358f5ba5a0bbed082f1a0070d1b5224c5d9
theycallmeeuan <89062379+theycallmeeuan@users.noreply.github.com>
2025-01-27 16:12:10 +0800

    Add files via upload

---
+++ python_llm_autograder/python_llm_autograder/routing.py
@@ -0,0 +1,7 @@
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import django_plotly_dash.routing
+
+# application = ProtocolTypeRouter({
+
+# })
